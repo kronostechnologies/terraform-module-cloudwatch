0169d3b36774c4bf722e5297fe03c14f62429bf8
Simon V <vigx@users.noreply.github.com>
2018-03-09 12:11:29 -0500

    basic aws_waf monitor

---
+++ dashboard/widgets/aws_waf/waf.tf
@@ -0,0 +1,25 @@
+variable "web_acl" {
+  type = "string"
+}
+
+variable "width" {
+  default = 6
+}
+
+variable "height" {
+  default = 5
+}
+
+data "template_file" "tpl" {
+  template = "${file("${path.module}/waf.json")}"
+
+  vars {
+    width   = "${var.width}"
+    height  = "${var.height}"
+    web_acl = "${var.web_acl}"
+  }
+}
+
+output "output" {
+  value = "${join(",",data.template_file.tpl.*.rendered)}"
+}
